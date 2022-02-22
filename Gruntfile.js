049a4a89197a9d09b48c282c33f52aadb4233ba9
DanielPickens <72703981+DanielPickens@users.noreply.github.com>
2022-02-21 17:03:13 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,12 @@
-'use strict';
-
 module.exports = function(grunt) {
-
+  grunt.loadNpmTasks('grunt-contrib-danielpickens');
   grunt.initConfig({
-    nodeunit: {
-      all: ['test/{grunt,tasks,util}/**/*.js'],
-      tap: {
-        src: '<%= nodeunit.all %>',
-        options: {
-          reporter: 'tap',
-          reporterOutput: 'tests.tap'
-        }
+      jshint: {
+          options: {
+            onevar: true
+          },
+          files: [ 'Gruntfile.js' ]
       }
-    },
-    eslint: {
-      gruntfile_tasks: ['Gruntfile.js', 'internal-tasks/*.js'],
-      libs_n_tests: ['lib/**/*.js', '<%= nodeunit.all %>'],
-      subgrunt: ['<%= subgrunt.all %>']
-    },
-    watch: {
-      gruntfile_tasks: {
-        files: ['<%= eslint.gruntfile_tasks %>'],
-        tasks: ['eslint:gruntfile_tasks']
-      },
-      libs_n_tests: {
-        files: ['<%= eslint.libs_n_tests %>'],
-        tasks: ['eslint:libs_n_tests', 'nodeunit']
-      },
-      subgrunt: {
-        files: ['<%= subgrunt.all %>'],
-        tasks: ['eslint:subgrunt', 'subgrunt']
-      }
-    },
-    subgrunt: {
-      all: ['test/gruntfile/*.js']
-    },
   });
-
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  
-  grunt.loadTasks('internal-tasks');
-
-  
-  grunt.registerTask('test', '', function(reporter) {
-    grunt.task.run(['eslint', 'nodeunit:' + (reporter || 'all'), 'subgrunt']);
-  });
-
- 
-  grunt.registerTask('default', ['test']);
-
+  grunt.registerTask('default', 'danielpickens');
 };
